34b56627f2488501387e8f76246c423caa4601ea
G00405094 <G00405094@atu.ie>
2023-11-08 14:52:08 +0000

    Initialised project

---
+++ src/main/java/ie/atu/warehouseservice/WarehouseService.java
@@ -0,0 +1,14 @@
+package ie.atu.warehouseservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WarehouseService {
+    private String warehouseId;
+    private String location;
+    private String capacity;
+}
